33074dff6ee661a91bb9c447ea50c4dd5414deed
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-01-10 18:00:50 +0100

    ci: setup build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,1 @@
-export { FirebaseFunctionsRateLimiter } from "./FirebaseFunctionsRateLimiter";
-export { FirebaseFunctionsRateLimiterConfiguration } from "./FirebaseFunctionsRateLimiterConfiguration";
-export { FirestoreEquivalent } from "./types/FirestoreEquivalent";
-export { RealtimeDbEquivalent } from "./types/RealtimeDbEquivalent";
-
-import { FirebaseFunctionsRateLimiter } from "./FirebaseFunctionsRateLimiter";
-export default FirebaseFunctionsRateLimiter;
+export * from "./Resource";
---
+++ src/main.ts
@@ -0,0 +1,2 @@
+export * from "./Resource";
+export { default as StatefulResource } from "./StatefulResource.vue";
